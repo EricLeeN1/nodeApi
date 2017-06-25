65edf5fdd7891c26edfc19b81a40b4a1dd2f967d
EricLeeN1 <179310009@qq.com>
2017-06-25 12:52:54 +0800

    001

---
+++ day03/filename.js
@@ -0,0 +1,11 @@
+// __filename:返回当前模块文件的解析后的绝对路径，该属性其实并非全局的，而是模块作用域下的
+// __dirname:返回当前模块文件所在目录解析后的绝对路径，该属性也不是全局的，而是模块作用域下的
+
+console.log(__filename);
+console.log(__dirname);
+
+// var d = new Date();
+setInterval(function() {
+    var d = new Date();
+    console.log('现在是:' + d.getFullYear() + '年' + (d.getMonth() + 1) + '月' + d.getDate() + '日' + d.getMinutes() + ':' + d.getSeconds());
+}, 1000)
