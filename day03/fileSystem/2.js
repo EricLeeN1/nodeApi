4c2110f6e1c83605a0193e6e66ac221c2082c87d
EricLeeN1 <179310009@qq.com>
2017-06-26 14:56:05 +0800

    001

---
+++ day03/fileSystem/2.js
@@ -0,0 +1,9 @@
+// var fs = require('fs');
+// // fs.open('1.txt', 'r', function(err, fd) {
+// //     console.log(err);
+// //     console.log(fd);
+// // });
+// // console.log('ok');
+
+// // var fd = fs.openSync('1.txt', 'r');
+// // console.log(fd);
---
+++ day03/fileSystem/3.js
@@ -0,0 +1,33 @@
+var fs = require('fs');
+
+fs.open('1.txt', 'r+', (err, fd) => {
+
+    //当我们要对打开的文件进行写操作的时候，打开文件的模式应该是 读写
+    if (err) {
+        console.log(err);
+    } else {
+        //读取文件
+        // fs.read(fd,buffer,offset,length,position,callback)
+        // fd:通过open方法成功打开一个文件返回的编号
+        // buffer:buffer对象
+        // offset：新的内容添加到buffer中的起始位置
+        //length:添加到buffer中的内容的长度
+        // postion： 读取的文件中的起始位置
+        // callback
+        //    err
+        //    buffer的长度
+        //    buffer对象
+        var bf1 = new Buffer('123');
+        // fs.read(fd, bf1, 0, 3, 5, (err, length, buf) => {
+        //     if (err) {
+        //         console.log(err);
+        //     } else {
+        //         console.log(buf.toString());
+        //         // console.log(length);
+        //         // console.log(buf);
+        //         // console.log(arguments)
+        //     }
+        // })
+        fs.write(fd, '1234', 5, 'utf-8');
+    }
+})
