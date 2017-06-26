6ca3d7a5f387410e7cc714ed510be554ab9b9dfc
EricLeeN1 <179310009@qq.com>
2017-06-26 16:22:13 +0800

    001

---
+++ day03/fileSystem/7.js
@@ -0,0 +1,12 @@
+var fs = require('fs');
+
+// fs.mkdir(path,[mode],callback)
+// fs.mkdir('./1', function(err) {
+//     console.log(arguments);
+// });
+// fs.rmdir('./1', function(err) {
+//     console.log(arguments);
+// });
+// fs.readdir('../Buffer', function(err, fileList) {
+//     console.log(fileList);
+// });
