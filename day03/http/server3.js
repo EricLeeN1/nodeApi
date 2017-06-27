e5d657f5ccbf61bc0a4142cb5e7f6c60056362fc
EricLeeN1 <179310009@qq.com>
2017-06-27 14:04:58 +0800

    001

--- day03/http/server3.js
+++ day03/http/server3.js
@@ -2,6 +2,7 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs');
 var server = http.createServer();
+var querystring = require('querystring');
 
 server.on('request', function(req, res) {
     var urlStr = url.parse(req.url);
@@ -14,6 +15,29 @@ server.on('request', function(req, res) {
         case '/user':
             //用户首页
             sentData(htmlDir + 'user.html', req, res);
+            break;
+        case '/login':
+            //用户首页
+            sentData(htmlDir + 'login.html', req, res);
+            break;
+        case '/login/check':
+            //用户首页
+            // console.log(req.method);
+            // console.log(urlStr);
+            // console.log(querystring.parse(urlStr.query));
+            // sentData(htmlDir + 'login.html', req, res);
+            if (req.method.toUpperCase() == 'POST') {
+                var str = '';
+                req.on('data', function(chunk) {
+                    str += chunk;
+                });
+                req.on('end', function() {
+                    console.log(str);
+                    console.log(querystring.parse(str));
+                })
+            } else {
+
+            }
             break;
         default:
             //处理其他情况
