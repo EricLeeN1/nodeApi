3ad4b7cdee858b27c7387c322cb4b7ad7354c4f3
EricLeeN1 <179310009@qq.com>
2017-07-16 11:37:29 +0800

    001

---
+++ day01/02_staticRender.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Eric on 2017/7/13.
+ */
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(req, res) {
+    if (req.url == '/fang') {
+        fs.readFile('./test.html', function(err, data) {
+            res.writeHead(200, { 'Content-Type': 'text/html;charset=UTF-8' });
+            res.end(data);
+        });
+    } else if (req.url == '/yuan') {
+        fs.readFile('./haha.html', function(err, data) {
+            res.writeHead(200, { 'Content-Type': 'text/html;charset=UTF-8' });
+            res.end(data);
+        });
+    } else {
+        res.writeHead(400, { 'Content-Type': 'text/html;charset=UTF-8' });
+        res.end('没有这个页面呦！');
+    }
+
+
+
+});
+server.listen(3000, '127.0.0.1');
