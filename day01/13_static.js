eed5fc1e9772fc0d0fad580792df9ecf42787145
EricLeeN1 <179310009@qq.com>
2017-07-17 14:25:30 +0800

    001

--- day01/13_static.js
+++ day01/13_static.js
@@ -5,23 +5,23 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs');
 var path = require('path');
-http.createServer(function (req, res) {
+http.createServer(function(req, res) {
     // 的到用户的路径
     var pathname = url.parse(req.url).pathname;
     //真的读取这个文件
-    if (pathname=='/') {
+    if (pathname == '/') {
         pathname = 'index.html';
     }
     //拓展名
     var extname = path.extname(pathname);
     console.log(extname);
-    fs.readFile('./static/'+pathname,function (err,data) {
+    fs.readFile('./static/' + pathname, function(err, data) {
         if (err) {
             // 如果子文件不存在，就应该用404返回
             // throw  err;
-            fs.readFile('./static/404.html',function (err,data) {
-               res.writeHead(404,{'Content-type':'text/html;charset=UTF8'});
-               res.end(data);
+            fs.readFile('./static/404.html', function(err, data) {
+                res.writeHead(404, { 'Content-type': 'text/html;charset=UTF8' });
+                res.end(data);
             });
             return;
         };
@@ -29,17 +29,17 @@ http.createServer(function (req, res) {
         //网页文件:  text/html
         //jpg文件: image/jpg
         var mime = getMime(extname);
-        res.writeHead(200,{'Content-type':mime});
+        res.writeHead(200, { 'Content-type': mime });
         res.end(data);
     });
 }).listen(3000, '127.0.0.1');
 
 function getMime(extname) {
-    switch (extname){
+    switch (extname) {
         case '.html':
             return 'text/html';
             break;
-        case '.jpg'||'.jpeg':
+        case '.jpg' || '.jpeg':
             return 'image/jpeg';
             break;
         case '.png':
@@ -55,6 +55,6 @@ function getMime(extname) {
             return 'application/x-javascript';
             break;
         default:
-                break;
+            break;
     }
 }
--- day02/03.js
+++ day02/03.js
@@ -1,10 +1,50 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
+var path = require('path');
 
 var server = http.createServer(function(req, res) {
-    fs.readFile('/static./index.html', function(err, data) {
-        res.writeHead(200, { 'Content-type': 'text/html;charset=UTF8' });
+    var pathname = url.parse(req.url).pathname;
+    if (pathname.indexOf('.') == -1) {
+        path += '/index.html';
+    }
+    console.log(pathname);
+    var fileUrl = './' + path.normalize('./static/' + pathname);
+    var extname = path.extname(fileUrl);
+    console.log(fileUrl, path.normalize('./static/' + pathname));
+    fs.readFile(fileUrl, function(err, data) {
+        if (err) {
+            res.writeHead(404, { 'Content-type': 'text/html;charset=UTF8' });
+            res.end('404页面没有找到');
+        }
+        var mime = getMime(extname);
+        res.writeHead(200, { 'Content-type': mime });
         res.end(data);
-    })
+    });
 });
-server.listen(80, '127.0.0.1');
+server.listen(3000, '127.0.0.1');
+
+function getMime(extname) {
+    switch (extname) {
+        case '.html':
+            return 'text/html';
+            break;
+        case '.jpg' || '.jpeg':
+            return 'image/jpeg';
+            break;
+        case '.png':
+            return 'image/png';
+            break;
+        case '.gif':
+            return 'image/gif';
+            break;
+        case '.css':
+            return 'image/css';
+            break;
+        case '.js':
+            return 'application/x-javascript';
+            break;
+        default:
+            break;
+    }
+}
