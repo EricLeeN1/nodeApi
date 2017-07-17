1611aed80441dce1177b89a6d8f1f482b583e6ea
EricLee <179310009@qq.com>
2017-07-17 21:26:03 +0800

    001

--- day02/03.js
+++ day02/03.js
@@ -6,45 +6,32 @@ var path = require('path');
 var server = http.createServer(function(req, res) {
     var pathname = url.parse(req.url).pathname;
     if (pathname.indexOf('.') == -1) {
-        path += '/index.html';
+        pathname += '/index.html';
     }
     console.log(pathname);
     var fileUrl = './' + path.normalize('./static/' + pathname);
-    var extname = path.extname(fileUrl);
-    console.log(fileUrl, path.normalize('./static/' + pathname));
+    var extname = path.extname(pathname);
+    // console.log(fileUrl, path.normalize('./static/' + pathname));
     fs.readFile(fileUrl, function(err, data) {
         if (err) {
-            res.writeHead(404, { 'Content-type': 'text/html;charset=UTF8' });
+            res.writeHead(404,{'Content-type':'text/html;charset=UTF8'});
             res.end('404页面没有找到');
         }
-        var mime = getMime(extname);
-        res.writeHead(200, { 'Content-type': mime });
-        res.end(data);
+        getMime(extname,function (mime) {
+            res.writeHead(200,{'Content-type':mime});
+            res.end(data);
+        });
     });
 });
 server.listen(3000, '127.0.0.1');
 
 function getMime(extname) {
-    switch (extname) {
-        case '.html':
-            return 'text/html';
-            break;
-        case '.jpg' || '.jpeg':
-            return 'image/jpeg';
-            break;
-        case '.png':
-            return 'image/png';
-            break;
-        case '.gif':
-            return 'image/gif';
-            break;
-        case '.css':
-            return 'image/css';
-            break;
-        case '.js':
-            return 'application/x-javascript';
-            break;
-        default:
-            break;
-    }
+    fs.readFile('./mime.json',function (err,data) {
+        if (err) {
+            throw  Error('找不到json文件');
+        }//转成json
+        var mineJson = JSON.parse(data);
+        console.log(mineJson[extname]);
+        return mineJson[extname];
+    });
 }
