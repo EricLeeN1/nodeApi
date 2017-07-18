0dea7d58b2a47b5e235bfa6d18e630190becda88
EricLee <179310009@qq.com>
2017-07-18 23:17:18 +0800

    001

---
+++ day02/router.js
@@ -0,0 +1,15 @@
+exports.showInfo = function (req, res) {
+    res.writeHead(200, {'Content-type': 'text/html;charset=UTF8'});
+    res.end('我是首页');
+};
+
+exports.showStudent = function (req, res) {
+    var id = req.url.substr(9, 6);
+    res.writeHead(200, {'Content-type': 'text/html;charset=UTF8'});
+    res.end('我是学生页面' + id);
+};
+
+exports.show404 = function (req, res) {
+    res.writeHead(404, {'Content-type': 'text/html;charset=UTF8'});
+    res.end('404');
+};
