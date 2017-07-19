df206dc1a78f7123010d0e83a62508cfd92e6932
EricLeeN1 <179310009@qq.com>
2017-07-19 13:53:39 +0800

    001

--- day02/11.js
+++ day02/11.js
@@ -2,15 +2,25 @@
  * Created by Eric on 2017/7/18.
  */
 var http = require('http');
+var querystring = require('querystring');
 
-var server = http.createServer(function (req, res) {
+var server = http.createServer(function(req, res) {
     // 如果访问地址是这个，并且请求方式是post
     if (req.url == '/dopost' && req.method.toLowerCase() == 'post') {
         var alldata = '';
-        req.addListener('data',function (chunk) {
-            alldata +=chunk;
+        req.addListener('data', function(chunk) {
+            alldata += chunk;
             console.log(alldata.toString());
-        })
+        });
+        req.addListener('end', function() {
+            var datastring = alldata.toString();
+            res.end('success');
+            //将string转为一个对象
+            var dataObj = querystring.parse(datastring);
+            console.log(dataObj.name);
+            console.log(dataObj);
+            console.log(dataObj.sex);
+        });
     }
 });
-server.listen(80, '127.0.0.1');
+server.listen(888, '127.0.0.1');
--- day02/list.md
+++ day02/list.md
@@ -4,3 +4,9 @@
  4. 模块演示,引用了test文件夹中的foo.js，学习暴露的知识
  5. 模块，引用了一个类，test中的People.js
  6. 如果不写./ 那么默认是node_modules文件夹文件
+ 7. 引用文件夹
+ 8. npm演示，下载了一个silly-datetime
+ 9. 模块中的路径
+ 10. router是外置的一个js文件，让程序可读性强
+ 11. post 请求的提交
+ 12.  
