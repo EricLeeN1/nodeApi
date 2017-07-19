2be933c92c5c38c635def1241220d11410485511
EricLee <179310009@qq.com>
2017-07-19 23:13:46 +0800

    001

---
+++ day02/12.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Eric on 2017/7/18.
+ */
+var http = require('http');
+var querystring = require('querystring');
+var formidable = require('formidable');
+
+var server = http.createServer(function(req, res) {
+    // 如果访问地址是这个，并且请求方式是post
+    if (req.url == '/dopost' && req.method.toLowerCase() == 'post') {
+        // parse a file upload
+        var form = new formidable.IncomingForm();
+        //执行里面回调函数的时候表单已经全部接收完毕了
+        form.parse(req, function(err, fields, files) {
+            console.log(fields);
+            console.log(files);
+            // 所有的文本域，单选框都在fields存放；
+            // 所有的文件域都在files存放；
+            res.writeHead(200, {'content-type': 'text/plain'});
+            res.write('received upload:\n\n');
+            res.end('成功');
+        });
+    }
+    // show a file upload form
+    // res.writeHead(200, {'content-type': 'text/html'});
+    // res.end(
+    //     '<form action="/upload" enctype="multipart/form-data" method="post">'+
+    //     '<input type="text" name="title"/><br/>'+
+    //     '<input type="file" name="upload" multiple="multiple"/><br/>'+
+    //     '<input type="submit" value="Upload"/>'+
+    //     '</form>'
+    // );
+});
+server.listen(888, '127.0.0.1');
