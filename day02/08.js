8e9bf2181481276e8b3f4c54ec43a376729d0b0e
EricLee <179310009@qq.com>
2017-07-20 21:51:46 +0800

    001

--- day02/08.js
+++ day02/08.js
@@ -1,3 +1,5 @@
 /**
  * Created by Eric on 2017/7/17.
  */
+var sd = require('silly-datetime');
+var ttt = sd.format(new Date(),'YYYYMMDDHHMM');
--- day02/13.js
+++ day02/13.js
@@ -5,26 +5,29 @@ var http = require('http');
 var querystring = require('querystring');
 var formidable = require('formidable');
 var util = require('util');
+var fs = require('fs');
 
-var server = http.createServer(function(req, res) {
+var server = http.createServer(function (req, res) {
     // 如果访问地址是这个，并且请求方式是post
     if (req.url == '/dopost' && req.method.toLowerCase() == 'post') {
         // parse a file upload
         var form = new formidable.IncomingForm();
         // 设置文件上传存放的路径
-        form.uploadDir='./uploads';
+        form.uploadDir = './uploads';
         //执行里面回调函数的时候表单已经全部接收完毕了
-        form.parse(req, function(err, fields, files) {
+        form.parse(req, function (err, fields, files) {
             if (err) {
                 throw  err;
             }
-            console.log(fields);
-            console.log(files);
+            console.log(util.inspect({fields: fields, files: files}));
+            // 执行改名
+            var oldPath = _dirname + '/' + files.tupian.path;
+            var newPath = _dirname + '/' +
             // 所有的文本域，单选框都在fields存放；
             // 所有的文件域都在files存放；
             res.writeHead(200, {'content-type': 'text/plain'});
             res.write('received upload:\n\n');
-            res.end(util.inspect({fields:fields,files:files}));
+            res.end(util.inspect({fields: fields, files: files}));
         });
     }
     // show a file upload form
