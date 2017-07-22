db972723c316c6135011c50bc1980e02f86ef802
EricLeeN1 <179310009@qq.com>
2017-07-22 15:21:14 +0800

    001

--- day03/01.js
+++ day03/01.js
@@ -4,4 +4,14 @@ var app = express();
 app.get('/', function(req, res) {
     res.send('你好');
 });
+app.get('/haha', function(req, res) {
+    res.send('这是哈哈页面，哈哈哈哈哈');
+});
+app.get(/^\/student\/([\d]{10})$/, function(req, res) {
+    res.send('学生信息:学号' + req.params[0]);
+});
+app.get("/teacher/:gonghao", function(req, res) {
+    // 正则表达式判断
+    res.send('老师信息:工号' + req.params["gonghao"]);
+});
 app.listen(3000);
