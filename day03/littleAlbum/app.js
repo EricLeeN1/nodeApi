24e3f1fa826d49056d8b6cdf881b2c6db4fa7146
EricLeeN1 <179310009@qq.com>
2017-07-24 14:02:33 +0800

    001

--- day03/littleAlbum/app.js
+++ day03/littleAlbum/app.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
 var router = require('./controller/router');
+
+// 设置模板引擎
 app.set('view engine', 'ejs');
 
-//路由中间件
-app.use(express.static('./public'));
+// 路由中间件
+// 静态页面
+app.use('/static', express.static('./public'));
+// 首页
 app.get('/', router.showIndex);
+app.listen(3000);
