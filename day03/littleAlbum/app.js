6cb83353c652934515eefebea53e3cb7fd46fa1c
EricLeeN1 <179310009@qq.com>
2017-07-24 13:51:11 +0800

    001

--- day03/littleAlbum/app.js
+++ day03/littleAlbum/app.js
@@ -1,3 +1,8 @@
 var express = require('express');
-
 var app = express();
+var router = require('./controller/router');
+app.set('view engine', 'ejs');
+
+//路由中间件
+app.use(express.static('./public'));
+app.get('/', router.showIndex);
