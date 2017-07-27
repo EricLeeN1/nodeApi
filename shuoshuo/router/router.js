4e6376071082832b9d8dc44f233cedc21041afa0
EricLee <179310009@qq.com>
2017-07-27 23:36:58 +0800

    001

--- shuoshuo/router/router.js
+++ shuoshuo/router/router.js
@@ -1,6 +1,17 @@
 /**
  * Created by Eric on 2017/7/27.
  */
+var formidable = require('formidable');
+//显示首页
 exports.showIndex=function (req,res,next) {
     res.render('index');
+};
+//注册页面
+exports.showRegister=function (req,res,next) {
+    res.render('register');
+};
+//注册业务
+exports.checkRegister=function (req,res,next) {
+    //得到用户的填写信息
+    //查询数据库中是否存在此人，不存在则添加
 };
